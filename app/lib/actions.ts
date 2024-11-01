de691b9bfb4ec2a5b10d473376f329c902def228
theing <934320370@qq.com>
2024-11-01 16:15:04 +0800

    chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,21 +7,47 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',  // 自定义错误信息
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',  // 自定义错误信息
+    }),
     date: z.string(),
 });
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
 // create操作
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 // 从 form表单中提取数据
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {  //prevState - 包含从 useActionState 钩子传递的状态。您不会在本示例的 action 中使用它，但它是必需的 prop。
+    // export async function createInvoice(formData: FormData) {
+    // 使用数据安全验证表单数据
+    const validatedFields = CreateInvoice.safeParse({  // safeParse（） 将返回一个包含 success 或 error 字段的对象。这将有助于更优雅地处理验证，而无需将此 logic 放入 try/catch 块中。
+        // const { customerId, amount, status } = CreateInvoice.parse({
         // const rawFormData = {
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+    // 如果 validatedFields 不成功，我们会提前返回函数，并显示来自 Zod 的错误消息。
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+    // 使用安全数据写入数据库
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;  // 以 cents 存储值
     const date = new Date().toISOString().split('T')[0];  // 创建日期
     try {  // 使用try catch 捕获错误
@@ -45,12 +71,20 @@ export async function createInvoice(formData: FormData) {
 
 // update 操作
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(prevState: State, id: string, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
+        // const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
 
     const amountInCents = amount * 100;
     try {
@@ -70,15 +104,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // delete 操作
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');  // 用于测试错误捕获的功能。
-    // try {
-    //     await sql`DELETE FROM invoices WHERE id = ${id}`;
-    //     revalidatePath('/dashboard/invoices');
-    //     return { message: "Deleted Invoice" };
-    // } catch (error) {
-    //     return {
-    //         message: "Database Error: Faild to Delete Invoice",
-    //     };
-    // }
+    // throw new Error('Failed to Delete Invoice');  // 用于测试错误捕获的功能。
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: "Deleted Invoice" };
+    } catch (error) {
+        return {
+            message: "Database Error: Faild to Delete Invoice",
+        };
+    }
 
 }
